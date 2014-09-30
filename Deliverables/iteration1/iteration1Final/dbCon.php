43b01c5136566de6f783d83bee297de848868093
kwegener <kristina.m.wegener@gmail.com>
2014-09-30 19:31:24 -0400

    removed error viewing

--- Deliverables/iteration1/iteration1Final/dbCon.php
+++ Deliverables/iteration1/iteration1Final/dbCon.php
@@ -1,8 +1,8 @@
 <?php 
 
-	ini_set('display_errors',1);
-	ini_set('display_startup_errors',1);
-	error_reporting(-1);
+	//ini_set('display_errors',1);
+	//ini_set('display_startup_errors',1);
+	//error_reporting(-1);
 		
 	$hostname = "172.31.35.123";
 	$username = "kristi";
