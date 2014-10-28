9b137b524db5e5d6582d9e129b946a54c035747f
kwegener <kristina.m.wegener@gmail.com>
2014-10-28 19:19:42 -0400

    added session info

--- import-vp.php
+++ import-vp.php
@@ -1,16 +1,15 @@
 <?php 
-	//ini_set('display_errors',1);
-	//ini_set('display_startup_errors',1);
-	//error_reporting(-1);
+	ini_set('display_errors',1);
+	ini_set('display_startup_errors',1);
+	error_reporting(-1);
 	
 	include "dbCon.php";
 
-	define('CSV_PATH','/var/www/html/csc440/uploadFiles/'); // specify CSV file path
+	define('CSV_PATH','/var/www/html/csc440/uploadFiles'); // specify CSV file path
     $csv_file = CSV_PATH . "vps.csv"; // Name of your CSV file
 	$csvfile = fopen($csv_file, 'r');
 	$theData = fgets($csvfile);
 	if(!$theData) echo "File not found";
-	else echo "Attempting to Add...<br>";
 	while (!feof($csvfile)) {
 		$fgetsline = fgets($csvfile);
 		echo $fgetsline."<br>";
@@ -23,21 +22,20 @@
 		$xcoord = trim($csv_array[10]);
 		$ycoord = trim($csv_array[11]);
 		$parcel = trim($csv_array[12]);
-		$parcel = substr($parcel, 1);
 		$nhood = trim($csv_array[13]);
 			
 		if($type!=NULL && $type!="NOTATION" && $st_no!=NULL && $st_name!=NULL && $parcel!=NULL && $nhood!=NULL) {
 		
 			//If there is a new Status, add it.
 			$StatCheckQuery = "SELECT * FROM vp_stat WHERE stat_name='$status'";
-			$StatCheckResult=mysqli_query($link, $StatCheckQuery);
+			$StatCheckResult=mysqli_query($con, $StatCheckQuery);
 			$num_rows = mysqli_num_rows($StatCheckResult);
 			if($num_rows==0 and $status!=NULL){
 				$statInsertQuery = "INSERT INTO vp_stat (stat_name) VALUES ('$status')";
-				mysqli_query($link, $statInsertQuery);
+				mysqli_query($con, $statInsertQuery);
 			}
 			$getStatQuery = "SELECT stat_id FROM vp_stat WHERE stat_name='$status'";
-			$statIDResult=mysqli_query($link, $getStatQuery);
+			$statIDResult=mysqli_query($con, $getStatQuery);
 			while ($row = mysqli_fetch_assoc($statIDResult)) {
 				$status=$row['stat_id'];
 			}
@@ -45,35 +43,35 @@
 			
 			//If there is a new type, add it
 			$TypeCheckQuery = "SELECT * FROM vp_type WHERE type_name='$type'";
-			$TypeCheckResult=mysqli_query($link, $TypeCheckQuery);
+			$TypeCheckResult=mysqli_query($con, $TypeCheckQuery);
 			$num_rows = mysqli_num_rows($TypeCheckResult);
 			if($num_rows==0 and $type!=NULL){
 				$TypeInsertQuery = "INSERT INTO vp_type (type_name) VALUES ('$type')";
-				mysqli_query($link, $TypeInsertQuery);
+				mysqli_query($con, $TypeInsertQuery);
 			}
 			$getTypeQuery = "SELECT type_id FROM vp_type WHERE type_name='$type'";
-			$typeIDResult=mysqli_query($link, $getTypeQuery);
+			$typeIDResult=mysqli_query($con, $getTypeQuery);
 			while ($row = mysqli_fetch_assoc($typeIDResult)) {
 				$type=$row['type_id'];
 			}
 			
 			//If there is a new address, add it
 			$AddrCheckQuery = "SELECT * FROM address WHERE parcel='$parcel'";
-			$AddrCheckResult=mysqli_query($link, $AddrCheckQuery);
+			$AddrCheckResult=mysqli_query($con, $AddrCheckQuery);
 			$num_rows = mysqli_num_rows($AddrCheckResult);
 			if($num_rows==0 and $parcel!=NULL){
 				$AddrInsertQuery = "INSERT INTO address (parcel, st_no, st_dir, st_name, nhood_id, xcoord, ycoord) VALUES
 				('$parcel', '$st_no', '$st_dir', '$st_name', $nhood, $xcoord, $ycoord)";
-				mysqli_query($link, $AddrInsertQuery);
+				mysqli_query($con, $AddrInsertQuery);
 			}
 			
-			$VPcheckQuery = "SELECT * FROM vp_list WHERE parcel='$parcel'";
-			$VPcheckResult=mysqli_query($link, $VPcheckQuery);
-			$num_rows = mysqli_num_rows($VPcheckResult);
+			$SRcheckQuery = "SELECT * FROM vp_list WHERE parcel='$parcel'";
+			$SRcheckResult=mysqli_query($con, $SRcheckQuery);
+			$num_rows = mysqli_num_rows($SRcheckResult);
 			if($num_rows==0){
-					$insertQuery = "INSERT INTO vp_list (parcel , type_id, stat_id) VALUES
-					('$parcel', $type, $status)";
-					mysqli_query($link, $insertQuery);
+					$insertQuery = "INSERT INTO vp_list ( parcel) VALUES
+					('$csr', $status, $type, '$desc', '$rcvd_dt', $priority, NULL, NULL, '$parcel')";
+					mysqli_query($con, $insertQuery);
 			}
 				//echo $insertQuery."<br>";
 		}
