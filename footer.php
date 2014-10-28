e4690e664809a1e49dba6671a8c00006893e3a97
kwegener <kristina.m.wegener@gmail.com>
2014-10-28 18:02:49 -0400

    added footer with admin login

--- footer.php
+++ footer.php
@@ -1,2 +1,13 @@
+<hr>
+<table border=0 width=100%>
+	<tr>
+		<td width=50%>
+			<p><a href="admin.php">Administrator Login Page</a></p>
+		</td>
+		<td width=50% align="right">
+			<p>2014 AKN</p>
+		</td>
+	</tr>
+</table>
 </body>
 </html>
