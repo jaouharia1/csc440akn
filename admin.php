16d22db9fbeb8a564c29b6ee04a011dd03bd6d7b
kwegener <kristina.m.wegener@gmail.com>
2014-10-28 18:02:38 -0400

    added header, minor formatting

--- admin.php
+++ admin.php
@@ -1,10 +1,11 @@
-<?php
 
-?>
 <html>
+	<?php
+	include "header.php";
+	?>
 	<form action='login.php' method='POST'>
-		Username	:<input type='text' name='username'><br>
-		Password	:<input type='password' name='password'><br>
-		<input type='submit' value='log in'><br>
+		Username: <input type='text' name='username'><br>
+		Password: <input type='password' name='password'><br>
+		<input type='submit' value='Log In'><br>
 	</form>
 </html> 
