31f892c795c1cb53cf8b5dbc25897e148eb96a4b
kwegener <kristina.m.wegener@gmail.com>
2014-11-05 18:53:38 -0500

    small update to main page

--- main.php
+++ main.php
@@ -1,5 +1,22 @@
-<?php
-	echo "<hr>";
-	echo "<h1>Main Page</h1><hr>";
-	echo "Welcome to our site!<br><br>";
-?>
+div {
+    font-family: "arial", sans-serif;
+}
+mainLogo {
+    font-family: "arial", sans-serif;
+	font-size: 40px;
+	
+}
+<div>
+<hr>
+<table>
+	<tr>
+		<td>
+			<p align='right'><a href="http://www.opendatacincy.org/"><img src="images/odcLogo.png"></a></p>
+		</td>
+		<td valign="bottom">
+			<p class="mainLogo">Connecting <font color="blue">People</font> with <font color="blue">Data</font></p>
+		</td>
+	</tr>
+</table>
+<br><br>
+</div>
