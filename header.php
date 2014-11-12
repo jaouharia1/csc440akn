0423873207b793b7215252abdd87bc5ce09532e3
kwegener <kristina.m.wegener@gmail.com>
2014-11-11 19:45:14 -0500

    added css, slight formatting changed

--- header.php
+++ header.php
@@ -1,19 +1,59 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
 		<title>City of Cincinnati</title>
-		<link href="city.css" rel="stylesheet" type="text/css" />
 		<link href="favicon.ico" rel="icon" type="images/x-icon" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style>
+
+		a:link { color: #00BCE4;
+				text-decoration: none;}
+		a:visited { color: #00457C;
+					text-decoration: none;}
+		a:hover { color: #CCCCCC;
+					font-weight: bold;
+					text-decoration: none;}
+		
+		
+		.right { text-align: right;
+				color: #00457C;
+		}
+		.title { text-align: left;
+				color: #00457C;
+				font-size: 30px;
+				font-weight: bold;
+		}
+		.mainLogo {
+			font-family: "arial", sans-serif;
+			font-size: 80px;	
+		}
+		.odcText {
+			font-family: "arial", sans-serif;
+			font-size: 15px;
+			color: #00457C;
+		}
+		div.padded {  
+			padding-top: 50px;  
+			padding-right: 100px;  
+			padding-bottom: 20px;  
+			padding-left: 100px;  
+		}
+		body {
+			background-image: url("images/swirl.jpg");
+			background-position: center top; 
+		}
+		
+		
+</style>
 	</head>
 <body>
 
 <table width=100%>
 	<tr>
-		<td><a href="index.php?pgNum=1"><img src="sr.png" alt="Service Requests"/></a></td>
-		<td><a href="index.php?pgNum=3"><img src="vp.png" alt="Vacant Properties"/></a></td>
-		<td><a href="index.php?pgNum=4"><img src="nhood.png" alt="Neighborhoods"/></a></td>
-		<td><a href="index.php?pgNum=5"><img src="map_stat.png" alt="Map View"/></a></td>
-		<td><a href="index.php"><img src="city_logo.png" alt="City of Cincinnati" align="right" /></a></td>
+		<td><a href="index.php?pgNum=1"><img src="images/sr.png" alt="Service Requests"/></a></td>
+		<td><a href="index.php?pgNum=3"><img src="images/vp.png" alt="Vacant Properties"/></a></td>
+		<td><a href="index.php?pgNum=4"><img src="images/nhood.png" alt="Neighborhoods"/></a></td>
+		<td><a href="index.php?pgNum=5"><img src="images/search.png" alt="Address Search"/></a></td>
+		<td><a href="index.php"><img src="images/city_logo.png" alt="City of Cincinnati" align="right" /></a></td>
 	</tr>
 </table>
 <?php
