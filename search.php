206acde8a5bcbb91a33ec673d9745bb83c8b2973
kwegener <kristina.m.wegener@gmail.com>
2014-11-11 19:47:09 -0500

    added bones for an address search page

---
+++ search.php
@@ -0,0 +1,7 @@
+<?php
+	echo "<hr>";
+	echo "<table width=100%><tr>";
+	echo "<td width=25% class='title'>Address Search</td>";
+	echo "<td width=75% class=\"right\">Search all Service Request Records and Vacant Property Records for an address</td></tr></table>";
+	echo "<hr>";
+?>
