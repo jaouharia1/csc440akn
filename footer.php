a92d5619a9122db0b1e707ab19587d90e587ba74
jaouharia1 <jaouharia1@nku.edu>
2014-11-12 16:38:16 -0500

    updating login links

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <table border=0 width=100%>
 	<tr>
 		<td width=50%>
-			<p><a href="admin.php">Administrator Login Page</a></p>
+			<p><a href="login.php">Administrator Login Page</a></p>
 		</td>
 		<td width=50% align="right">
 			<p class="right">2014 AKN</p>
