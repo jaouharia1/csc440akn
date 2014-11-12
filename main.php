05cfde3a4d88bfb00442059740d5795d1136acb8
kwegener <kristina.m.wegener@gmail.com>
2014-11-11 19:46:28 -0500

    added information about ODC and the site

--- main.php
+++ main.php
@@ -1,22 +1,16 @@
-div {
-    font-family: "arial", sans-serif;
-}
-mainLogo {
-    font-family: "arial", sans-serif;
-	font-size: 40px;
-	
-}
-<div>
 <hr>
+<div class="padded">
 <table>
 	<tr>
 		<td>
 			<p align='right'><a href="http://www.opendatacincy.org/"><img src="images/odcLogo.png"></a></p>
 		</td>
 		<td valign="bottom">
-			<p class="mainLogo">Connecting <font color="blue">People</font> with <font color="blue">Data</font></p>
+			<p class="mainLogo">Connecting<br><font color="blue">People</font> with <font color="blue">Data</font></p>
 		</td>
 	</tr>
 </table>
+<p class="odcText">OpenDataCincy is a portal that provides access to various data sets, applications, and APIs related to the Greater Cincinnati region. OpenDataCincy is based on the idea that providing free and easy access to data information encourages better and more transparent government and a more engaged and knowledgeable citizenry. By connecting people with data, OpenDataCincy is hoping to encourage users to take the data and transform it into creative applications, projects, and visualizations that demonstrate the power that data can have in understanding and shaping our communities.<br><br>
+This website was created with Data from OpenDataCincy, primarily the Non-Emergency Service Requests by year and the Vacant Property Records, by Ali Jaouhari and Kristina Wegener for Northern Kentucky University's Software Engineering class, Fall 2014. All data is assumed accurate as given.</p>
 <br><br>
 </div>
