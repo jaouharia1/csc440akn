b02be54131e0ce6ef1c01817b16e1bc5ff3498a4
Kristi <kristina.m.wegener@gmail.com>
2014-11-15 14:31:36 -0500

    added header, login info

--- getFiles.php
+++ getFiles.php
@@ -1,16 +1,37 @@
 <?php
-		file_put_contents("uploadFiles/sr-2008.xlsx", file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2008.xlsx"));
-		echo "Downloading 2008 Service Request Records...<br><br>";
-		file_put_contents("uploadFiles/sr-2009.xlsx", file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2009.xlsx"));
-		echo "Downloading 2009 Service Request Records...<br><br>";
-		file_put_contents("uploadFiles/sr-2010.xlsx", file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2010.xlsx"));
-		echo "Downloading 2010 Service Request Records...<br><br>";
-		file_put_contents("uploadFiles/sr-2011.xlsx", file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2011.xlsx"));
-		echo "Downloading 2011 Service Request Records...<br><br>";
-		file_put_contents("uploadFiles/sr-2012.xlsx", file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2012.xlsx"));
-		echo "Downloading 2012 Service Request Records...<br><br>";
-		file_put_contents("uploadFiles/sr-2013.xlsx", file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2013.xlsx"));
-		echo "Downloading 2013 Service Request Records...<br><br>";
+		include 'header.php';
+		include_once 'functions.php';
+		echo "<hr>";
+		$user = new User();
+		// Checking for user logged in or not
+		if ($user->get_session())
+		{
+			header("location:home.php");
+		}
+		
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2008.xlsx");
+		file_put_contents("uploadFiles/sr-2008.xlsx", $file);
+		echo "Downloading 2008 Service Request Records...<br>";
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2009.xlsx");
+		file_put_contents("uploadFiles/sr-2009.xlsx", $file);
+		echo "Downloading 2009 Service Request Records...<br>";
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2010.xlsx");
+		file_put_contents("uploadFiles/sr-2010.xlsx", $file);
+		echo "Downloading 2010 Service Request Records...<br>";
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2011.xlsx");
+		file_put_contents("uploadFiles/sr-2011.xlsx", $file);
+		echo "Downloading 2011 Service Request Records...<br>";
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2012.xlsx");
+		file_put_contents("uploadFiles/sr-2012.xlsx", $file);
+		echo "Downloading 2012 Service Request Records...<br>";
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2013.xlsx");
+		file_put_contents("uploadFiles/sr-2013.xlsx", $file);
+		echo "Downloading 2013 Service Request Records...<br>";
+		$file = file_get_contents("http://www.cincinnati-oh.gov/cityofcincinnati/assets/File/data/service_request_2014.xlsx");
+		file_put_contents("uploadFiles/sr-2014.xlsx", $file);
+		echo "Downloading 2014 Service Request Records...<br>";
 
-		echo "ALL RECORDS DOWNLOADED";
+		echo "ALL RECORDS DOWNLOADED<br><br>";
+		echo "<a href=\"home.php\">Return to Admin Console</a><br>";
+		include 'footer.php';
 ?>
