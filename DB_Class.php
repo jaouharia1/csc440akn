64751dfea99516fc5f61f919b40a6d8572eb1004
jaouharia1 <jaouharia1@nku.edu>
2014-11-14 19:57:59 -0500

    debugging

--- DB_Class.php
+++ DB_Class.php
@@ -1,10 +1,11 @@
 <?php
-include_once 'dbCon.php';
+
 class DB_Class
 {
 
     function __construct()
     {
+        include_once 'dbCon.php';
         $connection = mysql_connect($hostname, $username, $password) or
         die('Oops connection error -> ' . mysql_error());
         mysql_select_db($database, $connection)
