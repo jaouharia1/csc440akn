6b7e97219c64e0ab7d8ece6573779f030d361745
kwegener <kristina.m.wegener@gmail.com>
2014-11-14 19:40:25 -0500

    removed nadeen

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 			<p><a href="login.php">Administrator Login Page</a></p>
 		</td>
 		<td width=50% align="right">
-			<p class="right">2014 AKN</p>
+			<p class="right">2014 Ali & Kristi</p>
 		</td>
 	</tr>
 </table>
