d112be32d2e39807d635565a37eb2ca1a6302e3f
kwegener <kristina.m.wegener@gmail.com>
2014-11-17 17:56:06 -0500

    added script for mapping

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>City of Cincinnati</title>
 		<link href="favicon.ico" rel="icon" type="images/x-icon" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
 		<style>
 
 		a:link { color: #00BCE4;
@@ -43,7 +43,38 @@
 		}
 		
 		
-</style>
+		</style>
+		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+		<script>
+			var geocoder;
+			var map;
+			var index;
+		
+			function codeAddress() {
+				geocoder = new google.maps.Geocoder();
+				var latlng = new google.maps.LatLng(39.174208, -84.481842);
+				var mapOptions = {
+					zoom: 11,
+					center: latlng
+				}
+				map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+				
+				for	(index = 0; index < addrList.length; index++) {
+					geocoder.geocode( { 'address': addrList[index]}, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						var marker = new google.maps.Marker({
+							map: map,
+							position: results[0].geometry.location,
+							title: results[0].formatted_address
+						});
+					}
+					else {
+						alert("Geocode was not successful for the following reason: " + status);
+					}
+					});
+				} 
+			}
+		</script>
 	</head>
 <body>
 
