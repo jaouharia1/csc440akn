68d3af24aba2ef0be7f9dc5d69b8f4429fa2b131
kwegener <kristina.m.wegener@gmail.com>
2014-11-20 18:21:32 -0500

    re-separating. not sure how got back together

---
+++ DB_Class.php
@@ -0,0 +1,22 @@
+<?php
+class DB_Class 
+{
+
+    function __construct() 
+    {
+        /*$hostname = "172.31.17.39";
+	   $username = "kristi";
+	   $password = "password";
+	   $database = "akndb";*/
+	
+        global $hostname; 
+        global $username; 
+        global $password;
+        global $database;
+        $connection = mysql_connect($hostname, $username, $password) or 
+        die('Oops connection error -> ' . mysql_error());
+        mysql_select_db($database, $connection) 
+        or die('Database error -> ' . mysql_error());
+    }
+}
+?>
--- dbCon.php
+++ dbCon.php
@@ -28,29 +28,5 @@
 	}
 	$con=mysqli_real_connect($link, $hostname, $username, $password, $database);
 
-class DB_Class 
-{
-
-    function __construct() 
-    {
-        /*$hostname = "172.31.17.39";
-	   $username = "kristi";
-	   $password = "password";
-	   $database = "akndb";*/
-	
-        global $hostname; 
-        global $username; 
-        global $password;
-        global $database;
-        $connection = mysql_connect($hostname, $username, $password) or 
-        die('Oops connection error -> ' . mysql_error());
-        mysql_select_db($database, $connection) 
-        or die('Database error -> ' . mysql_error());
-    }
-}
-
-
-
-
 
 ?>
