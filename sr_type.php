e14a2f3de3d638c25e2fed43286aab576cf7981a
kwegener <kristina.m.wegener@gmail.com>
2014-11-20 17:52:13 -0500

    bad db connection

--- sr_type.php
+++ sr_type.php
@@ -5,13 +5,12 @@
 		public function __construct($type_id) {
 			$this->type_id = $type_id;
 		}
-		
-		
+			
 		public function getName() {
 			$name;
 			include "dbCon.php";
 			$query="SELECT type_name FROM sr_type WHERE type_id=$this->type_id";
-			$result = mysql_query($query, $con);
+			$result = mysql_query($link, $query);
 			while ($row = mysql_fetch_assoc($result)) {
 				$name=$row['type_name'];
 			}
