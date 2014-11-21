fba7477e5fb69ead3c813688ed0dab34a8a9223f
kwegener <kristina.m.wegener@gmail.com>
2014-11-20 20:24:06 -0500

    re-did to get file without neighborhoods

--- import-vp.php
+++ import-vp.php
@@ -1,91 +1,126 @@
 <?php 
-	ini_set('display_errors',1);
+	/*ini_set('display_errors',1);
 	ini_set('display_startup_errors',1);
-	error_reporting(-1);
+	error_reporting(-1);*/
 	
-	include "dbCon.php";
+	//include "dbCon.php";
 	include 'header.php';
 	include_once 'functions.php';
 	require_once 'PHPExcel/PHPExcel/IOFactory.php';
 	echo "<hr>";
-	$user = new User();
+	/*$user = new User();
 	// Checking for user logged in or not
 	if ($user->get_session())
 	{
 		header("location:home.php");
-	}
+	}*/
 	
-	file_put_contents("uploadFiles/vps.csv", file_get_contents("http://cagisperm.hamilton-co.org/cpop/vacant.csv"));
-	echo "Downloading Vacant Property Records...<br>";
-	define('CSV_PATH','/var/www/html/csc440/uploadFiles'); // specify CSV file path
-    $csv_file = CSV_PATH . "vps.csv"; // Name of your CSV file
+	define('CSV_PATH','/var/www/html/csc440/uploadFiles/'); // specify CSV file path
+    $csv_file = CSV_PATH . "vp.csv"; // Name of your CSV file
 	$csvfile = fopen($csv_file, 'r');
 	$theData = fgets($csvfile);
 	if(!$theData) echo "File not found";
+	$line=1;
 	while (!feof($csvfile)) {
 		$fgetsline = fgets($csvfile);
-		echo $fgetsline."<br>";
+		//echo $fgetsline."<br>";
 		$csv_array = explode(",", $fgetsline );
-		$type = trim($csv_array[0]);
-		$st_no = trim($csv_array[2]);
-		$st_name = trim($csv_array[3]);
-		$status = trim($csv_array[4]);
-		$st_dir = trim($csv_array[6]);
-		$xcoord = trim($csv_array[10]);
-		$ycoord = trim($csv_array[11]);
-		$parcel = trim($csv_array[12]);
-		$nhood = trim($csv_array[13]);
-			
-		if($type!=NULL && $type!="NOTATION" && $st_no!=NULL && $st_name!=NULL && $parcel!=NULL && $nhood!=NULL) {
+		$zero = trim($csv_array[0]);
+		$one = trim($csv_array[1]);
+		$two = trim($csv_array[2]);
+		$three = trim($csv_array[3]);
+		$four = trim($csv_array[4]);
+		$five = trim($csv_array[5]);
+		$six = trim($csv_array[6]);
+		$seven = trim($csv_array[7]);
+		$eight = trim($csv_array[8]);
+		$nine = trim($csv_array[9]);
+		if($line==1){
+			$type=$zero;
+			$wholeAddy=substr($one, 1);
+		}
+		elseif($line==3) {
+			$status=$three;
+			$parcel=substr($nine,1);
+		}
+		//echo "Testing line: type=".$type." addr=".$wholeAddy." status=".$status." parcel=".$parcel." line=".$line."<br>";
+		
+		if($type!=NULL && $wholeAddy!=NULL && $status!=NULL && $parcel!=NULL){
+			echo "Importing Record: ".$type.", ".$wholeAddy.", ".$status.", ".$parcel."<br>"; 
 		
 			//If there is a new Status, add it.
 			$StatCheckQuery = "SELECT * FROM vp_stat WHERE stat_name='$status'";
-			$StatCheckResult=mysqli_query($con, $StatCheckQuery);
+			$StatCheckResult=mysqli_query($link, $StatCheckQuery);
 			$num_rows = mysqli_num_rows($StatCheckResult);
 			if($num_rows==0 and $status!=NULL){
 				$statInsertQuery = "INSERT INTO vp_stat (stat_name) VALUES ('$status')";
-				mysqli_query($con, $statInsertQuery);
+				mysqli_query($link, $statInsertQuery);
 			}
 			$getStatQuery = "SELECT stat_id FROM vp_stat WHERE stat_name='$status'";
-			$statIDResult=mysqli_query($con, $getStatQuery);
+			$statIDResult=mysqli_query($link, $getStatQuery);
 			while ($row = mysqli_fetch_assoc($statIDResult)) {
 				$status=$row['stat_id'];
 			}
 			
-			
 			//If there is a new type, add it
 			$TypeCheckQuery = "SELECT * FROM vp_type WHERE type_name='$type'";
-			$TypeCheckResult=mysqli_query($con, $TypeCheckQuery);
+			$TypeCheckResult=mysqli_query($link, $TypeCheckQuery);
 			$num_rows = mysqli_num_rows($TypeCheckResult);
 			if($num_rows==0 and $type!=NULL){
 				$TypeInsertQuery = "INSERT INTO vp_type (type_name) VALUES ('$type')";
-				mysqli_query($con, $TypeInsertQuery);
+				mysqli_query($link, $TypeInsertQuery);
 			}
 			$getTypeQuery = "SELECT type_id FROM vp_type WHERE type_name='$type'";
-			$typeIDResult=mysqli_query($con, $getTypeQuery);
+			$typeIDResult=mysqli_query($link, $getTypeQuery);
 			while ($row = mysqli_fetch_assoc($typeIDResult)) {
 				$type=$row['type_id'];
 			}
 			
 			//If there is a new address, add it
 			$AddrCheckQuery = "SELECT * FROM address WHERE parcel='$parcel'";
-			$AddrCheckResult=mysqli_query($con, $AddrCheckQuery);
+			$AddrCheckResult=mysqli_query($link, $AddrCheckQuery);
 			$num_rows = mysqli_num_rows($AddrCheckResult);
+			$st_no=NULL;
+			$st_dir=NULL;
+			$st_name=NULL;
 			if($num_rows==0 and $parcel!=NULL){
-				$AddrInsertQuery = "INSERT INTO address (parcel, st_no, st_dir, st_name, nhood_id, xcoord, ycoord) VALUES
-				('$parcel', '$st_no', '$st_dir', '$st_name', $nhood, $xcoord, $ycoord)";
-				mysqli_query($con, $AddrInsertQuery);
+				$addrBroken=explode(" ", $wholeAddy);
+				$st_no=$addrBroken[0];
+				if($addrBroken[1]=="E" || $addrBroken[1]=="W" || $addrBroken[1]=="N" || $addrBroken[1]=="S"){
+					$st_dir=$addrBroken[1];
+					for($i=2; $i<sizeof($addrBroken); $i++) {
+						$st_name=$st_name." ".$addrBroken[$i];
+					}
+				}
+				else {
+					for($i=1; $i<sizeof($addrBroken); $i++) {
+						$st_name=$st_name." ".$addrBroken[$i];
+					}
+				}
+				//echo "Num: ".$st_no." dir: ".$st_dir." name: ".$st_name."<br>";
+				$AddrInsertQuery = "INSERT INTO address (parcel, st_no, st_dir, st_name) VALUES
+				('$parcel', '$st_no', '$st_dir', '$st_name')";
+				mysqli_query($link, $AddrInsertQuery);
 			}
 			
-			$SRcheckQuery = "SELECT * FROM vp_list WHERE parcel='$parcel'";
-			$SRcheckResult=mysqli_query($con, $SRcheckQuery);
-			$num_rows = mysqli_num_rows($SRcheckResult);
+			$VPcheckQuery = "SELECT * FROM vp_list WHERE parcel='$parcel'";
+			$VPcheckResult=mysqli_query($link, $VPcheckQuery);
+			$num_rows = mysqli_num_rows($VPcheckResult);
 			if($num_rows==0){
-					$insertQuery = "INSERT INTO vp_list ( parcel) VALUES
-					('$csr', $status, $type, '$desc', '$rcvd_dt', $priority, NULL, NULL, '$parcel')";
-					mysqli_query($con, $insertQuery);
+					$insertQuery = "INSERT INTO vp_list (parcel, type_id, stat_id) VALUES
+					('$parcel', $type, $status)";
+					mysqli_query($link, $insertQuery);
 			}
 				//echo $insertQuery."<br>";
+
+		}
+		$line++;
+		if($line==4) {
+			$status=NULL;
+			$parcel=NULL;
+			$type=NULL;
+			$wholeAddy=NULL;
+			$line=1;
 		}
 	}
     fclose($csvfile);
