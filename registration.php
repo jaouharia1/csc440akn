c87dccecab86d2f065a89f61639c6bac5ff420d7
jaouharia1 <jaouharia1@nku.edu>
2014-11-22 14:24:29 -0500

    registration debugging

--- registration.php
+++ registration.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     if ($register) 
     {
     // Registration Success
-        echo 'Registration successful <a href="login.php">Click here</a> to login';
+        echo 'Registration successful ';
         echo "<meta http-equiv=\"refresh\" content=\"3;url=home.php\" />";
     }
     else 
