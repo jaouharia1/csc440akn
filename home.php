11adae32585ac08b123089c4d5bb20f8c662f25e
Kristi <kristina.m.wegener@gmail.com>
2014-11-22 12:20:47 -0500

    added vp import link

--- home.php
+++ home.php
@@ -19,14 +19,14 @@ if (@$_GET['q'] == 'logout')
 <hr>
 <h1> Hello <?php $user->get_fullname($uid); ?>!</h1>
 <a href="registration.php">Add Administrator</a><br>
-<a href="getFiles.php">Download Service Request Files From ODC</a><br>
+<a href="getFiles.php">Download Service Request and Vacant Property Files From ODC</a><br>
 <a href="import-sr.php?year=2008">Import 2008 SRs From Downloaded File</a><br>
 <a href="import-sr.php?year=2009">Import 2009 SRs From Downloaded File</a><br>
 <a href="import-sr.php?year=2010">Import 2010 SRs From Downloaded File</a><br>
 <a href="import-sr.php?year=2011">Import 2011 SRs From Downloaded File</a><br>
 <a href="import-sr.php?year=2012">Import 2012 SRs From Downloaded File</a><br>
 <a href="import-sr.php?year=2013">Import 2013 SRs From Downloaded File</a><br>
-<a href="import-sr.php?year=2014">Import 2014 SRs From Downloaded File</a><br>
+<a href="import-vp.php">Import Vacant Property Records From Downloaded File</a><br>
 <br>
 <a href="?q=logout">LOGOUT</a></br>
 
