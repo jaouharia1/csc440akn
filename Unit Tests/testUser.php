75f5296f83917dfbd547ec89f176c03e6ecae699
jaouharia1 <jaouharia1@nku.edu>
2014-11-23 16:53:29 -0500

    Adding test case for class user

---
+++ Unit Tests/testUser.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once '../functions.php';
+
+class UserTest extends PHPUnit_Framework_TestCase
+{
+    protected $UserTest;
+    
+    protected function setUp()
+    {
+        $this->UserTest = new User;
+    }
+    
+    public function testRegistration()
+    {
+        $name = 'test user';
+        $username = 'testUser';
+        $password = 123456;
+        $passwordMD5 = "e10adc3949ba59abbe56e057f20f883e";
+        $email = 'test@phpunit.com';
+        echo "testing Registration";
+        $this->assertTrue(true, $this->UserTest->register_user($name, $username, $password, $email));
+        $this->assertFalse(false, $this->UserTest->register_user($name, $username, $password, $email));
+    }
+    
+    public function testLogin()
+    {
+        $name = 'test user';
+        $username = 'testUser';
+        $password = 123456;
+        $passwordMD5 = "e10adc3949ba59abbe56e057f20f883e";
+        
+        $this->assertTrue(true, $this->UserTest->check_login($username, $password));
+        $this->assertFalse(false, $this->UserTest->check_login($username, $password));
+        
+    }
+    public function testGet_fullname(){
+    
+        $uid=3;
+        $name = 'ali jaouhari';
+        $fakename = 'some string';
+        
+        $this->assertEquals($name, $this->UserTest->get_fullname($uid));
+        $this->assertNotEquals($fakename, $this->UserTest->get_fullname($uid));
+        $this->assertNotEquals($name, $this->UserTest->get_fullname($uid));//this suppose to fail
+    }
+
+
+
+}
+
+?>
