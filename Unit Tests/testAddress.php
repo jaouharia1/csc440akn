6f2b572e36236ba40a53345c570af0141b8dba44
jaouharia1 <jaouharia1@nku.edu>
2014-11-24 15:11:10 -0500

    Adding unit test for class address

---
+++ Unit Tests/testAddress.php
@@ -0,0 +1,34 @@
+<?php
+//require_once '/var/www/html/csc440/Unit Tests/PHPUnit-3.4.0/PHPUnit-3.4.0/PHPUnit/Framework/TestCase.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once '../functions.php';
+require_once '../address.php';
+
+class testAddress extends PHPUnit_Framework_Testcase
+{
+    protected $AddressTest;
+    
+    protected function setUp()
+    {
+        $parcel = 990020129;
+        $this->AddressTest = new address($parcel);
+    }
+    
+    public function testGet_Nhood()
+    {
+        $parcel = 990020129;
+
+        $nhood = "CUF";
+        $this->assertEquals($nhood, $this->AddressTest->getNhood());
+        $this->assertNotEquals($nhood, $this->AddressTest->getNood);
+    }
+    
+    public function testGet_Address()
+    {
+        $address = "W MCMICKEN AV";
+        $this->assertEquals($address, $this->AddressTest->getAddress());
+        
+    }
+}
+
+?>
