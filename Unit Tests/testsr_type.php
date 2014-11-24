340492dda318c486fd647db7865407bc3f12da2a
jaouharia1 <jaouharia1@nku.edu>
2014-11-24 16:56:15 -0500

    Adding test for vp_stat

---
+++ Unit Tests/testsr_type.php
@@ -0,0 +1,29 @@
+<?php
+//require_once '/var/www/html/csc440/Unit Tests/PHPUnit-3.4.0/PHPUnit-3.4.0/PHPUnit/Framework/TestCase.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once '../sr_type.php';
+
+class testSr_type extends PHPUnit_Framework_TestCase
+{
+    protected $sr_typeTest;
+    
+    protected function setUp()
+    {
+        $id=346;
+        $this->sr_typeTest = new sr_type($id);
+    }
+    
+    
+  
+    
+    public function testGetName()
+    {
+        $id193 = "Light repair";
+        $id223 = "Sign down/missing";
+        $id346 = "Default park";
+       // $this->assertEquals($id1, $this->sr_typeTest->getName());
+       // $this->assertEquals($id2, $this->sr_typeTest->getName());
+        $this->assertEquals($id346, $this->sr_typeTest->getName());
+    }
+}
+?>
