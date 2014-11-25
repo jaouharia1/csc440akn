c2676a774a7e58fac94919050a6c3f62f1370c02
jaouharia1 <jaouharia1@nku.edu>
2014-11-25 17:46:00 -0500

    Debugging test case for sr_stat.php

--- Unit Tests/testsr_stat.php
+++ Unit Tests/testsr_stat.php
@@ -21,19 +21,11 @@ class testSr_stat extends PHPUnit_Framework_TestCase
         $id1="CLOSED";
         $id2 = "ABAT-OWN";  
         $id3 ="NEW";       
-        $id4 = "VOIDED";    
-        $id5 = "CLOS-NO";  
-        $id6 = "ORDERS2";   
-        $id7 = "DUPLICAT"; 
-        $id8 = "ORDERS1";   
-        $id9 = "CIVIL";     
-        $id10= "CRIMINAL";  
-        $id11=  "ACCEPTED";  
-        $id12=  "ORDERS3";   
-        $id13=  "INPROGRS";  
-       // $this->assertEquals($id1, $this->sr_statTest->getName());
-       // $this->assertEquals($id2, $this->sr_statTest->getName());
+         
+        $this->assertNotEquals($id1, $this->sr_statTest->getName());
+        $this->assertNotEquals($id2, $this->sr_statTest->getName());
         $this->assertEquals($id3, $this->sr_statTest->getName());
+        
     }
 }
 ?>
