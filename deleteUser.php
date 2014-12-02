059b58a19946d24e2bc6b183ff98ab4ba54023d5
kwegener <kristina.m.wegener@gmail.com>
2014-12-02 18:13:50 -0500

    prevent XSS

--- deleteUser.php
+++ deleteUser.php
@@ -3,8 +3,9 @@ include 'header.php';
 include 'functions.php';
     $user = new User();
     
-    $toDelete = $user->get_fullname($_GET['uid']);
-    $query = "DELETE FROM users WHERE uid=". $_GET['uid'];
+	$uid = preg_replace('/[^0-9]/', '', $_GET['uid']);
+    $toDelete = $user->get_fullname($uid);
+    $query = "DELETE FROM users WHERE uid=". $uid;
     $result = mysqli_query($link, $query);
     echo $toDelete ." was successfully deleted";
     echo "<meta http-equiv=\"refresh\" content=\"3;url=home.php\" />";
